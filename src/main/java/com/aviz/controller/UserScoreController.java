f3f396742b1ee2912c39debb2007eee1b9fa7313
mtzcpd1485 <pedroaviz@cvccorp.com.br>
2020-05-31 18:22:09 -0300

    swagger added

--- src/main/java/com/aviz/controller/UserScoreController.java
+++ src/main/java/com/aviz/controller/UserScoreController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -28,12 +30,20 @@ public class UserScoreController {
 
     private final ScoreService rankingService;
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Create/Update user score. The points are added to the user’s"
+                    + " current score (score = current score + new points).") })
     @PostMapping(path = "/score", consumes = "application/json")
     public ResponseEntity<String> postScore(@RequestBody ScoreRequest score) {
         service.addPoints(score.getUserId(), score.getPoints());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, 
+            message = "Retrieves the current position of a specific user by it's userId,"
+                    + " considering the score for all users. If a userId was not found the response is empty.", 
+                    response = ScoreResponse.class) })
     @GetMapping(path = "/{userId}/position", produces = "application/json")
     public ResponseEntity<ScoreResponse> getPosition(@PathVariable Long userId) {
         Optional<ScoreResponse> optPos = rankingService.getUserPosition(userId);
@@ -43,6 +53,11 @@ public class UserScoreController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, 
+        message = "Retrieves the high scores list, in order, limited to the 20000 higher scores. "
+                + "Returns an empty list if no score has been submitted.", 
+                response = HighScoreListResponse.class) })
     @GetMapping(path = "/highscorelist", produces = "application/json")
     public ResponseEntity<HighScoreListResponse> getHighScoreList() {
         HighScoreListResponse response = HighScoreListResponse.builder().highscores(rankingService.getScoreList())
